c2e130f12d3a67940913705108768cd545943685
Ariaan <aryan.zalekian.83@gmail.com>
2025-02-06 23:04:44 +0330

    add more operators

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import tkinter
 from tkinter import *
 
+
+
+# main function
+def Callculator():
+    if Operator.get() == '+':
+        return Plus()
+    elif Operator.get() == '-':
+        return Minus()
+    elif Operator.get() == '*':
+        return Multiplication()
+    elif Operator.get() == '/':
+        return Division()
+    else:
+        Label.config(text='Invalid Operator')
+        return Label
 # + function
-def plus():
-    Title=(float(input.get()) + float(input2.get()))
+def Plus():
+    Title=(float(number1.get()) + float(number2.get()))
     Label.config(text=Title)
+# - function
+def Minus():
+    Title=(float(number1.get()) - float(number2.get()))
+    Label.config(text=Title)
+# * function
+def Multiplication():
+    Title=(float(number1.get()) * float(number2.get()))
+    Label.config(text=Title)
+# / function
+def Division():
+    if number2.get() == '0':
+        Label.config(text='Error: Division by zero')
+        return Label
+    else:
+        Title=(float(number1.get()) / float(number2.get()))
+        Label.config(text=Title)
 # reset button function
 def reset():
-    input.delete(0, END)
-    input2.delete(0, END)
+    number1.delete(0, END)
+    number2.delete(0, END)
+    Operator.delete(0, END)
     Label.config(text='')
+    
 
+#window
 window = Tk()
 title = window.title('calculator')
 window.geometry('500x500')
@@ -21,22 +55,26 @@ Label = Label(window,text ="Welcome to TK_calculator",fg="red",bg='light gray',f
 Label.pack()
 
 #button
-
-button = Button(window,text ="Calculate",bg='light gray',font=("Arial",10),command=plus)
+button = Button(window,text ="Calculate",bg='light gray',font=("Arial",10),command=Callculator)
 button.pack()
 
 button2 = Button(window,text ="Reset",bg='light gray',font=("Arial",10),command=reset)
 button2.pack()
 
 #inputs
-input = Entry(window)
-instruction_label =tkinter.Label(window, text="Input x:")
+number1 = Entry(window)
+instruction_label = tkinter.Label(window, text="Input x:")
 instruction_label.pack(pady=10)
-input.pack()
+number1.pack()
 
-input2 = Entry(window)
-instruction_label =tkinter.Label(window, text="Input y:")
+number2 = Entry(window)
+instruction_label = tkinter.Label(window, text="Input y:")
 instruction_label.pack(pady=10)
-input2.pack()
+number2.pack()
+
+Operator = Entry(window)
+instruction_label = tkinter.Label(window, text="Input Operator:(+,-,*,/)")
+instruction_label.pack(pady=10)
+Operator.pack()
 
 window.mainloop()
